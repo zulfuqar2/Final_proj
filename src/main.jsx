9ccfa89d4985ba106a629c8ed801035c1fa8f65f
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-06-01 15:21:48 +0400

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from '../src/state/store';
-import App from './App';
-
+import store from '../src/state/store.js';
+import App from './App'
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>,
   document.getElementById('root')
 );
